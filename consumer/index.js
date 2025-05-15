6453c26616b2cd928ea7bc2defadc6fd9c5d4010
pacman546 <91361260+pacman546@users.noreply.github.com>
2025-05-16 00:29:40 +0530

    Update index.js

--- consumer/index.js
+++ consumer/index.js
@@ -1,5 +1,6 @@
 import connectDB from '../config/db.js';
 import 'dotenv/config';
+import express from 'express';  
 import { connectAndConsume } from './services/mqConsumerService.js';
 import { connectRedis } from './config/redisConfig.js';
 import { connectRabbitMQ } from '../producer/services/mqService.js';
@@ -10,12 +11,23 @@ async function startConsumer() {
   try {
     await connectDB();
     await connectRedis();
-    await connectRabbitMQ(); // from producer
+    await connectRabbitMQ(); 
     await connectAndConsume();
 
-    startPendingDeliveryWorker(5); // every 5 minutes
+    startPendingDeliveryWorker(5);
     startPendingNotificationSaver();
     console.log('Consumer started and listening for messages...');
+
+    
+    const healthApp = express();
+    const HEALTH_PORT = process.env.HEALTH_PORT || 3001;
+
+    healthApp.get('/health', (req, res) => res.send('OK'));
+
+    healthApp.listen(HEALTH_PORT, () => {
+      console.log(`Healthcheck server listening on port ${HEALTH_PORT}`);
+    });
+
   } catch (error) {
     console.error('[x] Consumer failed to start:', error);
     process.exit(1);

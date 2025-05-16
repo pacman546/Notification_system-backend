30474c99435ee788b05c7571123383810a8d0b8c
pacman546 <91361260+pacman546@users.noreply.github.com>
2025-05-16 13:21:25 +0530

    add cors policy

--- producer/server.js
+++ producer/server.js
@@ -1,19 +1,23 @@
 import dotenv from 'dotenv';
 dotenv.config();
+
 import express from 'express';
+import cors from 'cors'; // ✅ Import CORS
+
 import connectDB from '../config/db.js';
 import { connectRabbitMQ } from './services/mqService.js';
 import likeRoutes from './routes/likeRoutes.js';
-import postRoutes from './routes/postRoutes.js'
-
+import postRoutes from './routes/postRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
 connectDB();
+
+app.use(cors());
 app.use(express.json());
 app.use('/like', likeRoutes);
 app.use('/posts', postRoutes);
-
 
 async function startServer() {
     await connectRabbitMQ();
